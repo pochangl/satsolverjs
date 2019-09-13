966e3f797eb3629a0a370385c1fb2e65cca85fc2
Pochang Lee <pochangl@usc.edu>
2019-09-13 13:55:09 +0800

    fill clauses to make sure there are clauses

--- src/utils/ast.ts
+++ src/utils/ast.ts
@@ -7,9 +7,16 @@ export interface IAbstractSyntaxTree {
   clauses: Array<IAbstractSyntaxTree>
 }
 const parser = Parser()
+
+function fill_clauses(tree: IAbstractSyntaxTree) {
+  tree.clauses = tree.clauses || []
+  tree.clauses.map(fill_clauses)
+  return tree
+}
+
 export function ast(str: string): IAbstractSyntaxTree {
   /*
     convert string to ast tree
   */
-  return parser.parse(str)
+  return fill_clauses(parser.parse(str))
 }
