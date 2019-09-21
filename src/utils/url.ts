16280857c0341eab21ab37c1c12666d10b394223
Pochang Lee <pochangl@usc.edu>
2019-09-22 02:30:09 +0800

    not needed for now

--- src/utils/url.ts
+++
@@ -1,9 +0,0 @@
-export function getUrlParams(): { [key: string]: string } {
-  const url = new URL(location.href)
-  const obj: { [key: string]: string } = {}
-
-  for (const key of url.searchParams.keys()) {
-    obj[key] = url.searchParams.get(key) as string
-  }
-  return obj
-}
