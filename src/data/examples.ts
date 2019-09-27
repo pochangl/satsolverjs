808c422b5b2cc53ed2bd117644f87a6d8946f9ae
Pochang Lee <pochangl@usc.edu>
2019-09-28 03:14:30 +0800

    package manager example

--- src/data/examples.ts
+++ src/data/examples.ts
@@ -11,16 +11,16 @@ const examples: IExample[] = [
       only 1 of {你出剪刀, 你出石頭, 你出布}
       only 1 of {我贏, 你贏, 平手}
 
-      我出剪刀 and 你出布  implies 我贏
-      我出石頭 and 你出剪刀  implies 我贏
-      我出布 and 你出石頭  implies 我贏
+      我出剪刀 and 你出布 implies 我贏
+      我出石頭 and 你出剪刀 implies 我贏
+      我出布 and 你出石頭 implies 我贏
 
-      你出剪刀 and 我出布  implies 你贏
-      你出石頭 and 我出剪刀  implies 你贏
-      你出布 and 我出石頭  implies 你贏
+      你出剪刀 and 我出布 implies 你贏
+      你出石頭 and 我出剪刀 implies 你贏
+      你出布 and 我出石頭 implies 你贏
 
-      我出剪刀 and 你出剪刀  implies 平手
-      我出石頭 and 你出石頭  implies 平手
+      我出剪刀 and 你出剪刀 implies 平手
+      我出石頭 and 你出石頭 implies 平手
       我出布 and 你出布 implies 平手
     `
   }, {
@@ -35,7 +35,6 @@ const examples: IExample[] = [
       only 1 of {7a, 7b, 7c, 7d, 7e, 7f, 7g, 7h}
       only 1 of {8a, 8b, 8c, 8d, 8e, 8f, 8g, 8h}
 
-
       only 1 of {1a, 2a, 3a, 4a, 5a, 6a, 7a, 8a}
       only 1 of {1b, 2b, 3b, 4b, 5b, 6b, 7b, 8b}
       only 1 of {1c, 2c, 3c, 4c, 5c, 6c, 7c, 8c}
@@ -44,7 +43,6 @@ const examples: IExample[] = [
       only 1 of {1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f}
       only 1 of {1g, 2g, 3g, 4g, 5g, 6g, 7g, 8g}
       only 1 of {1h, 2h, 3h, 4h, 5h, 6h, 7h, 8h}
-
 
       at most 1 of {1a}
       at most 1 of {2a, 1b}
@@ -77,6 +75,19 @@ const examples: IExample[] = [
       at most 1 of {6a, 7b, 8c}
       at most 1 of {7a, 8b}
       at most 1 of {8a}
+    `
+  }, {
+    name: 'Package manager',
+    text: `
+      only 1 of {A=1.0, A=1.1}
+      only 1 of {B=2.0, B=2.1, B=2.2, B=2.3, B=2.4}
+      only 1 of {C=3.0, C=3.1}
+
+      A=1.0 implies B=2.0 or B=2.1 or B=2.3 or B=2.4
+      A=1.1 implies B=2.1 or B=2.2 or B=2.4
+      C=3.0 implies A=1.0 or A=1.1 or B=2.0
+      C=3.1 implies A=1.1
+      C=3.1 implies B=2.4
     `
   }
 ]
